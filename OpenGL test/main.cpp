7c57b072e1abc55f6849efdf7de98b45d49e7e88
Sam Calthrop <sam@calthrop.com>
2024-03-16 20:58:03 +0000

    Initial Commit

---
+++ OpenGL test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGL test
+//
+//  Created by Sam Calthrop on 16/03/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
