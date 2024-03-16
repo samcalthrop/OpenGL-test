43df9de3f4cbfcb712325b232b32aa0d84a5ff58
Sam Calthrop <sam@calthrop.com>
2024-03-16 21:42:47 +0000

    setting things up

--- OpenGL test/main.cpp
+++ OpenGL test/main.cpp
@@ -1,14 +1,39 @@
-//
-//  main.cpp
-//  OpenGL test
-//
-//  Created by Sam Calthrop on 16/03/2024.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
+
+const GLint WIDTH = 800, HEIGHT = 600;
+
+int main() {
+    glfwInit();
+    
+    // idk really
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+//    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "test", nullptr, nullptr);
+
+    int screenWidth, screenHeight;
+    glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+    
+    if (nullptr == window) {
+        std::cout << "Failed to create window" << std::endl;
+        glfwTerminate();
+        
+        return -1;
+    }
+    
+    // set `window` to be the focus
+    glfwMakeContextCurrent(window);
+    
+    // include new "experimental" features
+    glewExperimental = GL_TRUE;
+    
+    
 }
